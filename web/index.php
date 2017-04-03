87a94e86e7d81edfe59cd131fc0de95fe4ec3bdd
Axel Freudiger <axel.freudiger@wetter.com>
2017-04-03 12:38:59 +0200

    caching, js loading appearence

--- web/index.php
+++ web/index.php
@@ -2,3 +2,6 @@
 require( dirname( __FILE__ ) . '/../src/application/views/header.phtml' );
 require( dirname( __FILE__ ) . '/../src/application/views/page.phtml' );
 require( dirname( __FILE__ ) . '/../src/application/views/footer.phtml' );
+
+//set caching header
+header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
