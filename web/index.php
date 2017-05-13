b3893b26645f4d0b70ef1fdcb189c1106307495e
Axel Freudiger <axel.freudiger@wetter.com>
2017-05-13 18:03:20 +0200

    page background-color

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
+//set caching header
+header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
 require( dirname( __FILE__ ) . '/../src/application/views/header.phtml' );
 require( dirname( __FILE__ ) . '/../src/application/views/page.phtml' );
 require( dirname( __FILE__ ) . '/../src/application/views/footer.phtml' );
-
-//set caching header
-header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
