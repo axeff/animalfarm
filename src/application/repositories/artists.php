f7ebcc0c1e907145caca731328689319fdb07216
Axel Freudiger <axel.freudiger@wetter.com>
2017-05-13 17:36:44 +0200

    centered page, new artists

--- src/application/repositories/artists.php
+++ src/application/repositories/artists.php
@@ -11,117 +11,151 @@ class Artists
   {
     $this->artists = [
       [
-        'name' => 'Aglaia',
+        'name' => 'aem.aze [defaultbox]',
+        'link' => 'https://soundcloud.com/aemaze',
+      ],
+      [
+        'name' => 'Aglaia [Springstoff]',
         'link' => 'https://soundcloud.com/djane-aglaia',
       ],
       [
-        'name' => 'Aki',
+        'name' => 'Aki [Animal Farm]',
       ],
       [
-        'name' => 'Bernstein Live',
-        'link' => 'https://soundcloud.com/bernstein_musik',
-      ],
-      [
-        'name' => 'Carli'
-      ],
-      [
-        'name' => 'Bernard Schiffbruch',
+        'name' => 'burnhard [Rummel I Schiffbruch]',
         'link' => 'https://soundcloud.com/burnhard-1',
       ],
       [
-        'name' => 'David José',
+        'name' => 'Bernstein [Arche Musik I Amselcom]',
+        'link' => 'https://soundcloud.com/bernstein_musik',
+      ],
+      [
+        'name' => 'Carli [Animal Farm]'
+      ],
+      // [
+      //   'name' => 'Bernard Schiffbruch',
+      //   'link' => 'https://soundcloud.com/burnhard-1',
+      // ],
+      [
+        'name' => 'David José & Christian Manuel [Zwischenmiete I Klickklackklub]',
         'link' => 'https://soundcloud.com/dav-dmolina',
       ],
       [
-        'name' => 'Don Lockwood',
+        'name' => 'Don Lockwood [Animal Farm]',
         'link' => 'https://soundcloud.com/donlockwood79',
       ],
       [
-        'name' => 'Franca',
+        'name' => 'Elo [Live-Rap]',
+      ],
+      [
+        'name' => 'Franca [Feines Tier I W.I.R Schwestern]',
         'link' => 'https://soundcloud.com/francaplays',
       ],
       [
-        'name' => 'Kleintierschaukel',
+        'name' => 'Frau Kaufmann [Berlin]',
+        'link' => 'https://soundcloud.com/frau-kaufmann',
+      ],
+      [
+        "name" => "Jackie Houser-Brown [W.I.R]",
+        "link" => "https://soundcloud.com/cocokillachemistry",
+      ],
+      [
+        "name" => "Jay Amba & Martin Ka [Zwischenmiete]",
+        "link" => "https://soundcloud.com/jayamba",
+      ],
+      [
+        "name" => "Just Emma [underyourskin]",
+        "link" => "https://soundcloud.com/justemmaoffical",
+      ],
+      [
+        'name' => 'Kleintierschaukel [Laut & Luise]',
         'link' => 'https://soundcloud.com/kleintierschaukel',
       ],
       [
-        'name' => 'Landhouse & Raddantze',
+        'name' => 'Landhouse & Raddantze [Seasidetrip I Serafin Audio]',
         'link' => 'https://soundcloud.com/landhouse',
       ],
       [
-        'name' => 'Lascha von Chapel',
+        'name' => 'Lascha von Chapel [Tbilisi]',
         'link' => 'https://soundcloud.com/lashachapel',
       ],
       [
-        'name' => 'Manel Cluny',
+        'name' => 'Manel Cluny [Animal Farm I Dazed State]',
         'link' => 'https://soundcloud.com/manelcluny',
       ],
       [
-        'name' => 'Marc Poppcke',
+        'name' => 'Marc Poppcke [Crossfrontier Audio]',
         'link' => 'https://soundcloud.com/marcpoppcke',
       ],
       [
-        'name' => 'Max Ravenstein',
+        "name" => "Martin Anacker [tanz+klangkombinat]",
+        "link" => "https://soundcloud.com/martin-anacker",
+      ],
+      [
+        "name" => "Marvin Hey [Sisyphos]",
+        "link" => "https://soundcloud.com/marvinhey",
+      ],
+      [
+        'name' => 'Max Gerolstein [Schwelgerei]',
         'link' => 'https://soundcloud.com/maxravenstein',
       ],
       [
-        'name' => 'Miguel Rodriguez + Tobi (Doyle Shepherd) + Steven Strahl',
+        'name' => 'Miguel Rodriguez + Tobi (Doyle Shepherd) + Steven Strahl [Golden Times]',
         'link' => 'https://soundcloud.com/miguelrodriguez',
       ],
       [
-        'name' => 'Nikkes',
-        'link' => 'https://soundcloud.com/nikkeskoeln',
+        "name" => "Moji Tali [Rebellion der Träumer I Akasha]",
+        "link" => "https://soundcloud.com/moji-taali",
       ],
       [
-        'name' => 'Niemand & Keiner + Sawatzki',
+        "name" => "Mr Pinc [Live-Rap]",
+      ],
+      [
+        'name' => 'Niemand & Keiner + Sawatzki [Rummel I Animal Farm I tanz + klangkombinat I Schiffbruch]',
         'link' => 'https://soundcloud.com/niemandundkeiner',
       ],
       [
-        'name' => 'Nig Näg & Häg Mäg',
+        'name' => 'Nig Näg & Häg Mäg [Zwischenmiete]',
         'link' => 'https://soundcloud.com/nignaeghaegmaeg',
       ],
       [
-        'name' => 'Nils Twachtmann',
+        'name' => 'Nils Twachtmann [Crossfrontier Audio, Bar 25]',
         'link' => 'https://soundcloud.com/nilstwachtmann',
       ],
       [
-        'name' => 'Oliver Klostermann',
+        'name' => 'Oliver Klostermann [Live Set]',
         'link' => 'https://soundcloud.com/oliver-klostermann',
       ],
       [
-        'name' => 'Raphael Hofman',
+        'name' => 'Raphael Hofman [Jeudi Records I Save Room I Wuza]',
         'link' => 'https://soundcloud.com/raphaelhofman',
       ],
       [
-        'name' => 'Rolandson',
+        'name' => 'Rolandson [Seasidetrip | Rituel Recording | Slowereastside | Drossel]',
         'link' => 'https://soundcloud.com/rolandson',
       ],
       [
-        'name' => 'Rusky',
+        'name' => 'Rusky [Animal Farm I Dazed State]',
         'link' => 'http://soundcloud.com/rusky',
       ],
       [
-        'name' => 'Saar:erer',
+        'name' => 'Saar:erer [Animal Farm I THC Franziskaner]',
         'link' => 'https://soundcloud.com/saarerer',
       ],
       [
-        'name' => 'Sauerkraut',
+        'name' => 'Sauerkraut [NE]',
         'link' => 'https://soundcloud.com/sauerkrautmusic',
       ],
       [
-        'name' => 'Steffen Kirchhof + Band',
+        "name" => "Stas Kolbass [Mehr tanzen!]",
+        "link" => "https://soundcloud.com/staskolbass",
+      ],
+      [
+        'name' => 'Steffen Kirchhof [Ki Records I Serafin]',
         'link' => 'https://soundcloud.com/steffenkirchhoff',
       ],
       [
-        'name' => 'Te-eM',
-        'link' => 'https://soundcloud.com/te-em-berlin',
-      ],
-      [
-        'name' => 'TmO',
-        'link' => 'https://soundcloud.com/tmo72',
-      ],
-      [
-        'name' => 'Vruno Live',
+        'name' => 'Vruno  [Nomade]',
         'link' => 'https://soundcloud.com/vrunomusic',
       ]
     ];
